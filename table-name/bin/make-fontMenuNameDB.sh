766c64ee7be2f18dc59115f0578b7df3f3067c75
Yoichiro MATSUMOTO <metasta@gmail.com>
2016-05-16 18:05:32 +0900

    Update make-fontMenuNameDB.sh (python3 -> perl)

--- table-name/bin/make-fontMenuNameDB.sh
+++ table-name/bin/make-fontMenuNameDB.sh
@@ -30,8 +30,8 @@ do
   esac
 done < "$fontinfo"
 
-unic=$(python3 -c 'import sys; print("".join([hex(ord(x)) for x in sys.argv[1]]).replace("0x","\\"))' "$fontnameja")
-sjis=$(python3 -c 'import sys; print("".join([hex(x) for x in sys.argv[1].encode("shift-jis")]).replace("0x","\\"))' "$fontnameja")
+unic=$(printf "$fontnameja" | perl -ne 'use Encode;foreach(split(//,decode_utf8($_))){printf"\\%x",ord($_)}')
+macj=$(printf "$fontnameja" | perl -ne 'use Encode;foreach(split(//,encode("MacJapanese",decode_utf8($_)))){printf"\\%x",ord($_)}')
 
 cat <<__EOT__
 [${psname}-${weight}]
@@ -43,7 +43,7 @@ cat <<__EOT__
   s=3,1,0x411,${weight}
   l=3,1,0x411,${unic} ${weight}
 
-  f=1,1,11,${sjis}
+  f=1,1,11,${macj}
   s=1,1,11,${weight}
-  l=1,1,11,${sjis} ${weight}
+  l=1,1,11,${macj} ${weight}
 __EOT__
